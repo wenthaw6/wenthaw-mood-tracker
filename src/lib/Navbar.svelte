a2e07fcee1dacb67e40d49535b87bb3bec8b996a
wenthaw6 <m-9430839@moe-dl.edu.my>
2021-11-21 07:26:15 +0000

    mode tracker

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-expand-xl navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">Mood Tracker</a>
+    </div>
+  </nav>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Navbar from '$lib/Navbar.svelte';
+</script>
+<Navbar/>
+<slot /> 
