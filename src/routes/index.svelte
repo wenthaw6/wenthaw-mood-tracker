fb9611ebdf02a3c356dd7c7e3a936602eb041d7e
wenthaw6 <m-9430839@moe-dl.edu.my>
2021-11-28 08:01:24 +0000

    Emoji

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,33 @@
 <script>
-    import Greeting from '$lib/Greeting.svelte'
-    import Entry from '$lib/Entry.svelte'
+    import Greeting from '$lib/Greeting.svelte';
+    import Entry from '$lib/Entry.svelte';
+    import EntryModal from '$lib/EntryModal.svelte';
 </script>
-<Greeting/>
+
+<Greeting />
 <!-- Entries -->
 <section class="container px-4 py-3">
     <div class="d-flex justify-content-between">
    	 <div class="p-2">Mood Log</div>
-   	 <input class="btn btn-light mb-2" type="button" value="+ New Entry" />
+   	 <input 
+        class="btn btn-light mb-2" 
+        type="button" 
+        value="+ New Entry" 
+        data-bs-toggle="modal"
+        data-bs-target="#newEntry"
+        />
     </div>
 
     <div class="list-group mb-3">
    	 <!-- Individual Entries -->
     	<Entry />
-
    	 
     </div>
 </section>
+
+<EntryModal />
+
+
+
 
 
